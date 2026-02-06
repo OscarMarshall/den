512ec289aa69edec17df9188bbc39a1fbcd907c5
Oscar Marshall <3111765+OscarMarshall@users.noreply.github.com>
2026-02-05 18:54:39 -0800

    Possibly missing Home-Managed User Context

---
+++ templates/examples/modules/_example/ci/user-parametric-hm-config.nix
@@ -0,0 +1,32 @@
+{ den, ... }:
+{
+  den.aspects.alice = {
+    includes = [
+      (
+        {
+          HM,
+          user,
+          host,
+        }:
+        let
+          unused = den.lib.take.unused [
+            HM
+            user
+            host
+          ];
+        in
+        {
+          homeManager.manual.html.enable = unused true;
+        }
+      )
+    ];
+  };
+
+  perSystem =
+    { checkCond, alice-at-rockhopper, ... }:
+    {
+      checks.alice-hm-manual-html-enabled-by-parametric-user = checkCond "parametric home-managed manual html for alice" (
+        alice-at-rockhopper.manual.html.enable
+      );
+    };
+}
